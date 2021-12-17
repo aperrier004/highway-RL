30915a33fbe835b2a0dc1f786492d96779ee9b80
Alban PERRIER <58979674+aperrier004@users.noreply.github.com>
2021-12-17 20:55:56 +0100

    Final version + add comments

--- highway.py
+++ highway.py
@@ -1,57 +1,99 @@
+# Author : Alban PERRIER (https://github.com/aperrier004/highway-RL)
+# Context : RL project at Bordeaux INP (ENSC - IA)
+# Goal : Implementation of DQN on the highway env (https://github.com/eleurent/highway-env)
+# Baseline : DQN exercice of Nathanael Fijalkow (https://github.com/nathanael-fijalkow/nathanael-fijalkow.github.io)
+
 import torch
-#from torch._C import ResolutionCallback
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 import numpy as np
 import gym
-from gym.spaces import Discrete, Box
-import random
+import math, random
 import highway_env
 
+# Import classes of models.py
 from models import DQN, Dueling_DQN
 
+# dll error, not mandatory on every environnements
+# https://stackoverflow.com/questions/20554074/sklearn-omp-error-15-initializing-libiomp5md-dll-but-found-mk2iomp5md-dll-a
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
+### Highway fast Environnement
 env = gym.make("highway-fast-v0")
 obs_dim = env.observation_space.shape[0]
 n_acts = env.action_space.n
 
-### Constants for training
-learning_rate = 1e-4
-epochs = 300
-batch_size = 32
-epsilon = 0.1
-env.config["duration"] = batch_size
-##########################
+### Epsilon greedy exploration
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 500
 
-#############################################
-####### BUILDING A NEURAL NETWORK ###########
-##### REPRESENTING ACTION STATE VALUES ######
-#############################################
+# Computes the epsilon for each episodes
+epsilon_by_epoch = lambda i: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * i / epsilon_decay)
 
+# Modify this to change the type of DQN
+model = "Dueling_DQN"
+
+###### Deep Q Network ############
 # net_Qvalue is a neural network representing an action state value function:
 # it takes as inputs observations and outputs values for each action
 net_Qvalue = DQN(1, n_acts)
 
 # net_Qvalue_target is another one
 net_Qvalue_target = DQN(1, n_acts)
+
+if model == "Dueling_DQN":
+    ### Dueling Deep Q Network
+    net_Qvalue = Dueling_DQN(1, n_acts)
+    net_Qvalue_target = Dueling_DQN(1, n_acts)
+
 net_Qvalue_target.eval()
 
+###### TRAINING ############
+### Constants for training
+learning_rate = 0.00001
+epochs = 100
+batch_size = 32
+#epsilon = 0.1 # Replaced by epsilon greedy exploration
 
 
-def choose_action(observation):
+def choose_action(observation, epsilon):
+    """Choose an action for an episode
+    
+    Args:
+        observation (float32 Tensor): the current state of the environnement with its conditions
+        
+    Returns:
+        q_values (int): value of the action to realize (either random or the maximum value of q_values[0])
+    """
     if random.random() < epsilon:
         return random.randrange(n_acts)
     else:
         with torch.no_grad():
+            # Concatenates observation tensors along  axis 0
             q_values = net_Qvalue(torch.stack([torch.stack([observation], axis=0)], axis=0))
             return q_values[0].max(0)[1].item()
 
 def compute_loss(batch_observations, batch_actions, batch_rewards, batch_next_observations, batch_non_final):
+    """Compute the loss for an epoch
+    
+    Args:
+        batch_observations (float32 Tensor): list of all the observations
+        batch_actions (float32 Tensor): list of all the actions
+        batch_rewards (float32 Tensor): list of all the reward
+        batch_next_observations (float32 Tensor): list of all the next observations
+        batch_non_final (float32 Tensor): list that counts the episodes not done
+        
+    Returns:
+        loss (int): value of the loss for the neural network
+    """
+    # Current model with batch_observations tensors concatenated along axis 1 
     batch_q_values = net_Qvalue(torch.stack([batch_observations], axis=1))
-    batch_q_value = batch_q_values.gather(0, batch_actions.unsqueeze(1)).squeeze(1)
+    # Target model
+    # Gathers values along the 0 axis 
+    batch_q_value = batch_q_values.gather(0, batch_actions.unsqueeze(1)).squeeze(1) # tensor with a dimension of size one inserted at the position 1 and then removed
     #print("q_values",batch_q_values.shape)
     #print("actions", len(batch_actions))
 
@@ -65,12 +107,14 @@ def compute_loss(batch_observations, batch_actions, batch_rewards, batch_next_ob
 
     batch_expected_q_value = batch_rewards + batch_q_value_next
     loss = (batch_q_value - batch_expected_q_value).pow(2).mean()
+
     return loss
 
 # make optimizer
 optimizer = Adam(net_Qvalue.parameters(), lr = learning_rate)
 
 def DQN():
+    """Training the Deep Q Network"""
     for i in range(epochs):
         # we copy the parameters of Qvalue into Qvalue_target every 10 iterations
         if i % 10 == 0:
@@ -93,7 +137,8 @@ def DQN():
         # First step: collect experience by simulating the environment using the current policy
         while True:
             old_observation = observation.copy()
-            action = choose_action(torch.as_tensor(observation, dtype=torch.float32))
+            epsilon = epsilon_by_epoch(i)
+            action = choose_action(torch.as_tensor(observation, dtype=torch.float32), epsilon)
             observation, reward, done, _ = env.step(action)
             total_reward += reward
 
@@ -128,21 +173,23 @@ def DQN():
         batch_loss.backward()
         optimizer.step()
 
-        #if i % 20 == 0:
         print('epoch: %3d \t loss: %.3f' % (i, batch_loss))
 
 DQN()
 
 ###### EVALUATION ############
 
-def run_episode(env, render = False):
+def run_episode(env, i, render = False):
+    """Testing cycle"""
     obs = env.reset()
     total_reward = 0
     done = False
     while not done:
         if render:
             env.render()
-        action = choose_action(torch.as_tensor(obs, dtype=torch.float32))
+            
+        epsilon = epsilon_by_epoch(i)
+        action = choose_action(torch.as_tensor(obs, dtype=torch.float32), epsilon)
         obs, reward, done, _ = env.step(action)
         total_reward += reward
         if done:
@@ -150,10 +197,10 @@ def run_episode(env, render = False):
     return total_reward
 
 epsilon = 0
-policy_scores = [run_episode(env) for _ in range(100)]
+policy_scores = [run_episode(env, i) for i in range(100)]
 print("Average score of the policy: ", np.mean(policy_scores))
 
-for _ in range(2):
-  run_episode(env, True)
+for i in range(10):
+  run_episode(env, i, True)
 
 env.close()
--- models.py
+++ models.py
@@ -1,57 +1,126 @@
+# Author : Alban PERRIER (https://github.com/aperrier004/highway-RL)
+# Conteq_valuest : RL project at Bordeauq_values INP (ENSC - IA)
+# Goal : Implementation of DQN on the highway env (https://github.com/eleurent/highway-env)
+
 import torch
 import torch.nn as nn
 import numpy as np
 
 class DQN(nn.Module):
+    """
+    A class used to create a Deep Q Network
+
+    Parameters
+    ----------
+    nn : Tensor
+        The neural network
+
+    Attributes
+    ----------
+    layers : Tensor
+        A sequential container
+
+    Methods
+    -------
+    forward(q_values)
+        Give the neural network
+    """
     def __init__(self, in_channels, num_actions):
+        """
+        Parameters
+        ----------
+        in_channels : int
+            The number of inputs channels
+        num_actions : int 
+            The number of actions
+        """
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=5, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1)
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 32, (5,5)), 
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_actions)
+        )
 
-        self.fc1 = nn.Linear(in_features=64, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=num_actions)
-
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    def forward(self, q_values):
+        """Choose an action for an epoch
+    
+        Args:
+            q_values (float32 Tensor): the neural network
+            
+        Returns:
+            q_values (float32 Tensor): Tensor list of the q values
+        """
+        return self.layers(q_values)
 
 class Dueling_DQN(nn.Module):
+    """
+    A class used to create a Dueling Deep Q Network
+
+    Parameters
+    ----------
+    nn : Tensor
+        The neural network
+
+    Attributes
+    ----------
+    layers : Tensor
+        A sequential container
+
+    Methods
+    -------
+    forward(q_values)
+        Give the neural network
+    """
     def __init__(self, in_channels, num_actions):
+        """
+        Parameters
+        ----------
+        in_channels : int
+            The number of inputs channels
+        num_actions : int 
+            The number of actions
+        """
         super(Dueling_DQN, self).__init__()
         self.num_actions = num_actions
         
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=5, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1)
+        self.conv_1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=5, stride=4)
+        self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2)
+        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1)
 
-        self.fc1_adv = nn.Linear(in_features=64, out_features=512)
-        self.fc1_val = nn.Linear(in_features=64, out_features=512)
+        self.advantage_1 = nn.Linear(in_features=64, out_features=512)
+        self.value_1 = nn.Linear(in_features=64, out_features=512)
 
-        self.fc2_adv = nn.Linear(in_features=512, out_features=num_actions)
-        self.fc2_val = nn.Linear(in_features=512, out_features=1)
+        self.advantage_2 = nn.Linear(in_features=512, out_features=num_actions)
+        self.value_2 = nn.Linear(in_features=512, out_features=1)
 
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        batch_size = x.size(0)
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = x.view(x.size(0), -1)
+    def forward(self, q_values):
+        """Choose an action for an epoch
+    
+        Args:
+            q_values (Tensor): the neural network
+            
+        Returns:
+            q_values (float32 Tensor): Tensor list of the q values
+        """
+        q_values = self.relu(self.conv_1(q_values))
+        q_values = self.relu(self.conv_2(q_values))
+        q_values = self.relu(self.conv_3(q_values))
+        # New tensor with the same data as the q_values tensor but of a shape 0.
+        q_values = q_values.view(q_values.size(0), -1)
 
-        adv = self.relu(self.fc1_adv(x))
-        val = self.relu(self.fc1_val(x))
+        advantages = self.relu(self.advantage_1(q_values))
+        values = self.relu(self.value_1(q_values))
 
-        adv = self.fc2_adv(adv)
-        val = self.fc2_val(val).expand(x.size(0), self.num_actions)
+        advantages = self.advantage_2(advantages)
+        # Returns a new view of the q_values tensor with singleton dimensions expanded to a larger size.
+        values = self.value_2(values).expand(q_values.size(0), self.num_actions)
         
-        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.num_actions)
-        return x
+        q_values = values + advantages - advantages.mean(1).unsqueeze(1).expand(q_values.size(0), self.num_actions)
+        return q_values
